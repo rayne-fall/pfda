3bc656ec558cf3c68633785257746b16ca454b1f
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2024-11-10 16:39:29 +0000

    completed simple version

--- assignments/assignment_5_risk.py
+++ assignments/assignment_5_risk.py
@@ -4,8 +4,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def round():
+a_wins=0
+d_wins=0
 
+def round():
+    global a_wins
+    global d_wins
+   
     # make dice array
     die = np.array([i for i in range(1,7)])
 
@@ -24,14 +29,29 @@ def round():
     # discard the attacker's lowest roll
     a_rolls=np.delete(a_rolls, 2)
 
-    # compare results to determine round winner (incomplete)
-'''
-    if a_rolls<=d_rolls:
-        print("Defence Wins!")
+    outcome=a_rolls>d_rolls
+    if np.all(outcome)==True:
+        a_wins= a_wins+1
     else:
-        print("Attack Wins!")
-'''
+        d_wins= d_wins+1
+  
+   
+for _ in range (1000):
+    round()
+
+
+# create labels for each section
+labels = "Attackers Win", "Defenders Win",
+results = [a_wins, d_wins]
+
+# generate pie chart with percentages labelled
+plt.pie(results, autopct='%1.1f%%')
+
+# add label and title
+plt.legend(labels, bbox_to_anchor=(0.75, 0, 0.5, 1))
+plt.show()
 
 # References:
 # https://stackoverflow.com/questions/26984414/efficiently-sorting-a-numpy-array-in-descending-order
 # https://www.geeksforgeeks.org/how-to-remove-specific-elements-from-a-numpy-array/
+# https://stackoverflow.com/questions/74412503/cannot-access-local-variable-a-where-it-is-not-associated-with-a-value-but
